e2e445f8fbd463cf007c28464a12e889dff749cf
DennySPB <dennyspb@gmail.com>
2019-09-27 15:21:57 +0300

    Launcher3: Add toggle for DT2S
    commit 39fa8b063cf682771c1800a2391689f96808218f
    Author: Omkar Chandorkar <gotenksIN@aosip.dev>
    Date:   Tue Feb 1 19:06:26 2022 +0530
    
        fixup! Launcher3: Add toggle for DT2S
    
        Change-Id: I2e537fa8641503d4ddfb10cad79e7d5fd0db3f2b
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I22abb02de7394094653d571e20eee0e93fb1e9a5
    
    [YAAP] fixup for 14

--- src/com/android/launcher3/touch/WorkspaceTouchListener.java
+++ src/com/android/launcher3/touch/WorkspaceTouchListener.java
@@ -50,6 +50,8 @@ import com.android.launcher3.testing.TestLogging;
 import com.android.launcher3.testing.shared.TestProtocol;
 import com.android.launcher3.util.TouchUtil;
 
+import com.android.launcher3.Utilities;
+
 /**
  * Helper class to handle touch on empty space in workspace and show options popup on long press
  */
@@ -79,14 +81,17 @@ public class WorkspaceTouchListener extends GestureDetector.SimpleOnGestureListe
 
     private final GestureDetector mGestureDetector;
 
+    private final Context mContext;
+
     public WorkspaceTouchListener(Launcher launcher, Workspace<?> workspace) {
         mLauncher = launcher;
         mWorkspace = workspace;
+        mContext = workspace.getContext();
         // Use twice the touch slop as we are looking for long press which is more
         // likely to cause movement.
         mTouchSlop = 2 * ViewConfiguration.get(launcher).getScaledTouchSlop();
-        mPm = (PowerManager) workspace.getContext().getSystemService(Context.POWER_SERVICE);
-        mGestureDetector = new GestureDetector(workspace.getContext(), this);
+        mPm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+        mGestureDetector = new GestureDetector(mContext, this);
     }
 
     @Override
@@ -223,7 +228,8 @@ public class WorkspaceTouchListener extends GestureDetector.SimpleOnGestureListe
 
     @Override
     public boolean onDoubleTap(MotionEvent event) {
-        mPm.goToSleep(event.getEventTime());
+        if (Utilities.isDoubleTapGestureEnabled(mContext))
+            mPm.goToSleep(event.getEventTime());
         return true;
     }
 }
