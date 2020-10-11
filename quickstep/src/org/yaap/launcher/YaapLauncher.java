6ad8f0ff15ccb2620b382577263fc079ceafdbc7
Anay Wadhera <awadhera@berkeley.edu>
2020-10-10 22:46:40 -0700

    Launcher3: Google feed integration
    Change-Id: I4dd635369513e7e8db8f8e4363acb017e0ce8eb1
    [kdrag0n: Debranded to com.android.launcher3]
    [kdrag0n: Ported to Android 12 and converted libGoogleFeed to Soong]
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- quickstep/src/org/yaap/launcher/YaapLauncher.java
+++ quickstep/src/org/yaap/launcher/YaapLauncher.java
@@ -17,7 +17,13 @@
 package org.yaap.launcher;
 
 import com.android.launcher3.uioverrides.QuickstepLauncher;
+import com.android.systemui.plugins.shared.LauncherOverlayManager;
 
 public class YaapLauncher extends QuickstepLauncher {
+
+    @Override
+    protected LauncherOverlayManager getDefaultOverlay() {
+        return new OverlayCallbackImpl(this);
+    }
 
 }
