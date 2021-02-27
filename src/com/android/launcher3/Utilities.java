d6868530ca76e26b935e6b3c6d37079558f179a0
Pranav Vashi <neobuddy89@gmail.com>
2021-02-27 10:00:38 +0530

    Launcher3: Delink font size settings
    idoybh: Adapted to our settings
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -134,6 +134,7 @@ public final class Utilities {
     public static final String KEY_NOTIFICATION_GESTURE = "pref_notification_gesture";
 
     public static final String ICON_SIZE = "pref_custom_icon_size";
+    public static final String FONT_SIZE = "pref_custom_font_size";
 
     /**
      * Indicates if the device has a debug build. Should only be used to store additional info or
@@ -709,6 +710,11 @@ public final class Utilities {
     public static int getIconSizeModifier(Context context) {
         SharedPreferences prefs = getPrefs(context.getApplicationContext());
         return prefs.getInt(ICON_SIZE, 100);
+    }
+
+    public static int getFontSizeModifier(Context context) {
+        SharedPreferences prefs = getPrefs(context.getApplicationContext());
+        return prefs.getInt(FONT_SIZE, 100);
     }
 
     public static boolean isNotificationGestureEnabled(Context context) {
