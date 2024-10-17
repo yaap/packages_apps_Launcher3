47872a8cd516ceab46aa8c9be253653cddc71084
Ido Ben-Hur <idoybh2@gmail.com>
2024-10-17 16:54:56 +0300

    Launcher3: Add an option to open the keyboard with the drawer

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -189,6 +189,7 @@ public final class Utilities {
     public static final String KEY_DOCK_SEARCH_PROVIDER = "pref_dock_search_provider";
     public static final String KEY_APP_DRAWER_OPACITY = "pref_app_drawer_opacity";
     public static final String KEY_FORCE_MONOCHROME_ICONS = "pref_forced_monochrome_icons";
+    public static final String KEY_AUTO_KEYABORD = "pref_auto_keyboard";
     public static final String KEY_BLUR_DEPTH = "pref_blur_depth";
     public static final String KEY_MINUS_ONE = "pref_enable_minus_one";
 
@@ -1001,6 +1002,11 @@ public final class Utilities {
     public static boolean enableMonoChromeThemedIcons(Context context) {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_FORCE_MONOCHROME_ICONS, false);
+    }
+
+    public static boolean enableAutoIme(Context context) {
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_AUTO_KEYABORD, false);
     }
 
     public static int getBlurRadius(Context context) {
