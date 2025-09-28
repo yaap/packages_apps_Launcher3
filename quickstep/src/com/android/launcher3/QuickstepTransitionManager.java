dfcfe59cd39ab7dda1878c7d6a9b79159cf096f5
Tracy Zhou <tracyzhou@google.com>
2025-09-27 20:45:52 -0700

    Launcher3: Should use unfilteredApps when getting Launcher target
    We changed to passing in openingTargets (that's added to the release check), so we need to use unfilteredApps here
    
    Bug: 379905951
    Test: manual
    Flag: com.android.launcher3.app_launch_blur
    Change-Id: Icae3aa2a71578d34c07f3629987011fb66071c88
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/launcher3/QuickstepTransitionManager.java
+++ quickstep/src/com/android/launcher3/QuickstepTransitionManager.java
@@ -1271,7 +1271,7 @@ public class QuickstepTransitionManager implements OnDeviceProfileChangeListener
         }
 
         RemoteAnimationTarget launcherTarget = null;
-        for (final RemoteAnimationTarget target : targets.apps) {
+        for (final RemoteAnimationTarget target : targets.unfilteredApps) {
             if (MODE_CLOSING == target.mode) {
                 launcherTarget = target;
                 break;
