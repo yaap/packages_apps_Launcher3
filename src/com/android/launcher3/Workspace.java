09be7ec7f8d2b8957ed5e554bf0c3865506c6e92
John Galt <johngaltfirstrun@gmail.com>
2025-12-30 12:16:18 -0500

    [HACK] Workspace: fix page indicator height on 3button

--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -406,15 +406,17 @@ public class Workspace<T extends View & PageIndicator> extends PagedView<T>
 
         // Set insets for page indicator
         Rect padding = grid.mWorkspaceProfile.getWorkspacePadding();
-        if (grid.isVerticalBarLayout()) {
+//        if (grid.isVerticalBarLayout()) {
             lp.leftMargin = padding.left + grid.mWorkspaceProfile.getWorkspaceCellPaddingXPx();
             lp.rightMargin = padding.right + grid.mWorkspaceProfile.getWorkspaceCellPaddingXPx();
             lp.bottomMargin = padding.bottom;
+/*
         } else {
             lp.leftMargin = lp.rightMargin = 0;
             lp.gravity = Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM;
             lp.bottomMargin = grid.hotseatBarSizePx;
         }
+*/
         pageIndicatorContainer.setLayoutParams(lp);
     }
 
