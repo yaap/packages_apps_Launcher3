c46b50554b8b479b3d9954540264a3b25f90d9ed
John Galt <johngaltfirstrun@gmail.com>
2026-02-25 14:29:29 -0500

    BaseAllAppsAdapter: silence bad logspam

--- src/com/android/launcher3/allapps/BaseAllAppsAdapter.java
+++ src/com/android/launcher3/allapps/BaseAllAppsAdapter.java
@@ -280,6 +280,7 @@ public abstract class BaseAllAppsAdapter
                                     privateProfileManager.getReadyToAnimate())
                                 && privateProfileManager.getCurrentState() == STATE_ENABLED
                                 ? 0 : 1);
+			/*
                         Log.d(TAG, "onBindViewHolder: "
                                 + "isPrivateSpaceItem: " + isPrivateSpaceItem
                         + " isStateTransitioning: " + privateProfileManager.isStateTransitioning()
@@ -287,6 +288,7 @@ public abstract class BaseAllAppsAdapter
                         + " readyToAnimate: " + privateProfileManager.getReadyToAnimate()
                         + " currentState: " + privateProfileManager.getCurrentState()
                         + " currentAlpha: " + icon.getAlpha());
+			*/
                     }
                     // Views can still be bounded before the app list is updated hence showing icons
                     // after collapsing.
