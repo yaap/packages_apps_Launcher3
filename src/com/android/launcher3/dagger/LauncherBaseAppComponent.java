ac5137e5d4849363f75fec810ad563980902153d
Tracy Zhou <tracyzhou@google.com>
2026-02-28 22:17:18 -0800

    Launcher3: Support blur in Launcher folders
    We use a combination of cross window blur and render effect of the pre open drag layer screenshot so that we can blend the blurred icons with the blurred wallpaper.
    
    Bug: 445467156
    Test: http://recall/-/cJXSFKMnb60bY3b9qyndqO/jzM1UnC8ULP9wDBUV9m8B
    Flag: com.android.launcher3.blur_on_more_surfaces
    Change-Id: I05cde2128015806bfc3a89677c945da615dab1a5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/dagger/LauncherBaseAppComponent.java
+++ src/com/android/launcher3/dagger/LauncherBaseAppComponent.java
@@ -57,6 +57,7 @@ import com.android.launcher3.util.DisplayController;
 import com.android.launcher3.util.DynamicResource;
 import com.android.launcher3.util.InstantAppResolver;
 import com.android.launcher3.util.LayoutImportExportHelper;
+import com.android.launcher3.util.ListenableRef;
 import com.android.launcher3.util.LockedUserState;
 import com.android.launcher3.util.MSDLPlayerWrapper;
 import com.android.launcher3.util.PackageManagerHelper;
@@ -75,6 +76,8 @@ import com.android.launcher3.widget.util.WidgetSizeHandler;
 import dagger.BindsInstance;
 
 import javax.inject.Named;
+
+import static com.android.launcher3.util.WindowBlurState.WINDOW_BLUR_STATE;
 
 /**
  * Launcher base component for Dagger injection.
@@ -148,6 +151,9 @@ public interface LauncherBaseAppComponent {
 
     /** Tracker for any app icon changes */
     IconChangeTracker getIconChangeTracker();
+
+    /** Tracker for cross window blur enabled state */
+    @Named(WINDOW_BLUR_STATE) ListenableRef<Boolean> getWindowBlurState();
 
     /** Builder for LauncherBaseAppComponent. */
     interface Builder {
