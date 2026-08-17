7f6854770b0bbbb1f371b975a26cee5ef0ad86fc
John Galt <johngaltfirstrun@gmail.com>
2026-08-17 11:24:12 -0400

    quickstep: add Bubble shortcut to TaskShortcuts

--- quickstep/src/com/android/launcher3/dagger/Modules.kt
+++ quickstep/src/com/android/launcher3/dagger/Modules.kt
@@ -336,6 +336,7 @@ object TaskOverlayModule {
     ): List<TaskShortcutFactory> =
         listOf(
             TaskShortcutFactory.APP_INFO,
+            TaskShortcutFactory.BUBBLE,
             TaskShortcutFactory.SPLIT_SELECT,
             TaskShortcutFactory.PIN,
             TaskShortcutFactory.INSTALL,
